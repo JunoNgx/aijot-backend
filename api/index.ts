7308907a6b490b1716a421a5d53c80de42ca196d
Juno Nguyen <juno.ngx@gmail.com>
2026-05-04 23:13:28 +0800

    refactor: remove commented HonoRequest patch, add edge runtime comment
    - Removed unused HonoRequest patch code (was causing issues on edge)
    - Added comment in api/index.ts explaining why we use edge runtime

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,8 @@
 import app from "../src/index"
 
+// Use edge runtime to avoid Vercel's nodejs runtime issue where the
+// Request object's headers property is not a Headers instance with .get().
+// See: https://github.com/sergiodxa/remix-i18next/issues/117
 export const config = { runtime: "edge" }
 
 export default app.fetch
